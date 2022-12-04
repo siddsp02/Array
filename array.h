8277780fbcd0d3818d37263b608efbe2b9d89206
Sidd <35938702+siddsp02@users.noreply.github.com>
2022-12-04 16:56:33 -0700

    Create array.h

---
+++ array.h
@@ -0,0 +1,68 @@
+/* Flexible dynamic array implementation in C. The following code is my own. */
+
+#ifndef ARRAY_H
+#define ARRAY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+typedef struct {
+    size_t size;
+    size_t elem_size;
+    size_t capacity;
+    void *data;
+} array;
+
+#define INITIAL_CAPACITY 8
+#define max(a, b) ((a > b) ? a : b)
+#define len(arr) (sizeof(arr) / sizeof(arr[0])) // For static arrays (in scope).
+
+// Type generic macros.
+
+#define arr_new(type) {                                         \
+    .size = 0,                                                  \
+    .elem_size = sizeof(type),                                  \
+    .capacity = INITIAL_CAPACITY,                               \
+    .data = malloc(INITIAL_CAPACITY * sizeof(type)),            \
+}
+#define arr_create(arr) ({                                      \
+    array ret = {                                               \
+        .size = len(arr),                                       \
+        .elem_size = sizeof(arr[0]),                            \
+        .capacity = max(INITIAL_CAPACITY, len(arr)),            \
+    };                                                          \
+    ret.data = malloc(ret.capacity * ret.elem_size);            \
+    memcpy(ret.data, arr, sizeof(arr));                         \
+    ret;                                                        \
+})
+#define arr_fill(val, len) ({                                   \
+    array ret = {                                               \
+        .size = len,                                            \
+        .elem_size = sizeof(val),                               \
+        .capacity = len,                                        \
+        .data = malloc(len * sizeof(val)),                      \
+    };                                                          \
+    for (size_t i = 0; i < len; ++i)                            \
+        ((typeof(val) *) ret.data)[i] = val;                    \
+    ret;                                                        \
+})
+#define arr_resize(arr, new_capacity) {                         \
+    arr.capacity = new_capacity;                                \
+    arr.data = realloc(arr.data, arr.capacity * arr.elem_size); \
+}
+#define arr_push(arr, elem) {                                   \
+    if (arr.capacity == arr.size)                               \
+        arr_resize(arr, arr.capacity*2)                         \
+    ((typeof(elem) *) arr.data)[arr.size++] = elem;             \
+}
+#define arr_insert(arr, i, elem) {                              \
+    if (arr.capacity == arr.size)                               \
+        arr_resize(arr, arr.capacity * 2);                      \
+    memmove(((typeof(elem) *) arr.data) + i + 1, (typeof(elem) *) arr.data, (arr.size++ - i) * sizeof(elem));\
+    ((typeof(elem) *) arr.data)[i] = elem;                      \
+}
+#define arr_dest(arr) free(arr.data);
+#define arr_qsort(arr, cmp) qsort(arr.data, arr.size, arr.elem_size, cmp);
+#define arr_bsearch(key, arr, cmp) bsearch(key, arr.data, arr.size, arr.elem_size, cmp);
+#endif /* ARRAY_H */
