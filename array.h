4127a90bee51d533d027b5820572f386f4419ead
Sidd <35938702+siddsp02@users.noreply.github.com>
2023-04-05 03:41:05 -0600

    Add `do ... while (0)` to allow for compound statements.

--- array.h
+++ array.h
@@ -1,4 +1,5 @@
-/* Flexible dynamic array implementation in C. The following code is my own. */
+/* Dynamic array implementation in C. The following code is my own borrowed
+   from https://github.com/siddsp02/ArrayLib/blob/main/array.h */
 
 #ifndef ARRAY_H
 #define ARRAY_H
@@ -27,25 +28,25 @@ typedef struct {
         ((type *) ret.data)[i] = val;                           \
     ret;                                                        \
 })
-#define arr_resize(arr, new_capacity) {                         \
+#define arr_resize(arr, new_capacity) do {                      \
     (arr)->capacity = new_capacity;                             \
     (arr)->data = realloc((arr)->data, (arr)->capacity * (arr)->elem_size); \
-}
-#define arr_push(arr, elem) {                                   \
+} while (0)
+#define arr_push(arr, elem) do {                                \
     if ((arr)->capacity == (arr)->size)                         \
         arr_resize(arr, (arr)->capacity * 2);                   \
     ((typeof(elem) *) (arr)->data)[(arr)->size++] = elem;       \
-}
+} while (0)
 // Make sure to dereference when popping (e.g. int x = *(int *) arr_pop(arr)).
 #define arr_pop(arr) (((char *) (arr)->data) + --(arr)->size * (arr)->elem_size)
-#define arr_insert(arr, i, elem) {                              \
+#define arr_insert(arr, i, elem) do {                           \
     if ((arr)->capacity == (arr)->size)                         \
         arr_resize(arr, (arr)->capacity * 2);                   \
     memmove(((typeof(elem) *) (arr)->data) + i + 1, ((typeof(elem) *) (arr)->data) + i, ((arr)->size++ - i) * sizeof(elem)); \
     ((typeof(elem) *) (arr)->data)[i] = elem;                   \
-}
-#define arr_remove_at(arr, i) memmove(((char *) (arr)->data) + i * (arr)->elem_size, ((char *) (arr)->data) + (i+1) * (arr)->elem_size, (--(arr)->size - i) * (arr)->elem_size);
-#define arr_dest(arr) free((arr)->data);
-#define arr_qsort(arr, cmp) qsort((arr)->data, (arr)->size, (arr)->elem_size, cmp);
-#define arr_bsearch(arr, key, cmp) bsearch(key, (arr)->data, (arr)->size, (arr)->elem_size, cmp);
+} while (0)
+#define arr_remove_at(arr, i) memmove(((char *) (arr)->data) + (i) * (arr)->elem_size, ((char *) (arr)->data) + ((i)+1) * (arr)->elem_size, (--(arr)->size - (i)) * (arr)->elem_size)
+#define arr_dest(arr) free((arr)->data)
+#define arr_qsort(arr, cmp) qsort((arr)->data, (arr)->size, (arr)->elem_size, cmp)
+#define arr_bsearch(arr, key, cmp) bsearch(key, (arr)->data, (arr)->size, (arr)->elem_size, cmp)
 #endif  /* ARRAY_H */
