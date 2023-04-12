6e80527d193b0561f815ce9263e25186a6f8d34e
Sidd <35938702+siddsp02@users.noreply.github.com>
2023-04-12 04:01:13 -0600

    Changed design to just use plain pointers.
    I got this idea from a video on modern C practices (https://youtu.be/QpAhX-gsHMs?t=1944).

--- array.h
+++ array.h
@@ -1,57 +1,49 @@
-/* Flexible dynamic array implementation in C. The following code is my own. */
-
 #ifndef ARRAY_H
 #define ARRAY_H
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-    size_t size, elemsize, capacity;
-    void *data;
-} array;
+typedef struct { size_t size, capacity; } _array;
 
-#define arr_new(type) (array) {                                 \
-    .elemsize = sizeof(type),                                   \
-    .capacity = 8,                                              \
-    .data = malloc(8 * sizeof(type)),                           \
-}
-#define arr_from_ptr(ptr, len) (array) {                        \
-    .elemsize = sizeof(ptr[0]),                                 \
-    .capacity = len,                                            \
-    .data = ptr,                                                \
-    .size = len,                                                \
-}
-#define arr_fill(type, val, len) ({                             \
-    array ret = {                                               \
-        .size = len,                                            \
-        .elemsize = sizeof(type),                               \
-        .capacity = len,                                        \
-        .data = malloc(len * sizeof(type)),                     \
-    };                                                          \
-    for (size_t i = 0; i < len; ++i)                            \
-        ((type *) ret.data)[i] = val;                           \
-    ret;                                                        \
+#define array(T) T *
+#define arr_data(arr) ((_array *) (arr) - 1) 
+#define arr_new(T) ({                                      \
+    char *ptr = malloc(sizeof(_array) + 8 * sizeof(T));    \
+    ptr += sizeof(_array);                                 \
+    arr_data(ptr)->size = 0;                               \
+    arr_data(ptr)->capacity = 8;                           \
+    (T *) ptr;                                             \
 })
-#define arr_resize(arr, new_capacity) do {                      \
-    (arr)->capacity = new_capacity;                             \
-    (arr)->data = realloc((arr)->data, (arr)->capacity * (arr)->elemsize); \
+#define arr_fill(T, val, len) ({\
+    char *ptr = malloc(sizeof(_array) + len * sizeof(T));  \
+    ptr += sizeof(_array);                                 \
+    arr_data(ptr)->size = 0;                               \
+    arr_data(ptr)->capacity = len;                         \
+    for (size_t i = 0; i < len; ++i)                       \
+        ptr[i] = val;                                      \
+    (T *) ptr;                                             \
+})
+#define len(arr) ({ arr_data(arr)->size; })
+#define arr_cap(arr) ({ arr_data(arr)->capacity; })
+#define arr_resize(arr, new_capacity) do {                 \
+    arr_data(arr)->capacity = new_capacity;                \
+    _array *tmp = realloc(arr_data(arr), sizeof(_array) + new_capacity * sizeof(*arr)); \
+    arr = (typeof(arr)) (((char *) tmp) + sizeof(_array)); \
 } while (0)
-#define arr_cast(type, arr) ((type *) (arr)->data)
-#define arr_push(arr, elem) do {                                \
-    if ((arr)->capacity == (arr)->size)                         \
-        arr_resize(arr, (arr)->capacity * 2);                   \
-    ((typeof(elem) *) (arr)->data)[(arr)->size++] = elem;       \
+#define arr_push(arr, elem) do {                           \
+    if (arr_cap(arr) == len(arr))                          \
+        arr_resize(arr, arr_cap(arr) * 2);                 \
+    arr[arr_data(arr)->size++] = elem;                     \
 } while (0)
-// Make sure to dereference when popping (e.g. int x = *(int *) arr_pop(arr)).
-#define arr_pop(arr) (((char *) (arr)->data) + --(arr)->size * (arr)->elem_size)
-#define arr_insert(arr, i, elem) do {                           \
-    if ((arr)->capacity == (arr)->size)                         \
-        arr_resize(arr, (arr)->capacity * 2);                   \
-    memmove(((typeof(elem) *) (arr)->data) + i + 1, ((typeof(elem) *) (arr)->data) + i, ((arr)->size++ - i) * sizeof(elem)); \
-    ((typeof(elem) *) (arr)->data)[i] = elem;                   \
+#define arr_pop(arr) ({ arr[--arr_data(arr)->size]; })
+#define arr_insert(arr, i, elem) do {                      \
+    if (arr_cap(arr) == len(arr))                          \
+        arr_resize(arr, arr_cap(arr) * 2);                 \
+    memmove(arr + i + 1, arr + i, (arr_data(arr)->size++ - i) * sizeof(*arr)); \
+    arr[i] = elem;                                         \
 } while (0)
-#define arr_remove_at(arr, i) memmove(((char *) (arr)->data) + (i) * (arr)->elem_size, ((char *) (arr)->data) + ((i)+1) * (arr)->elem_size, (--(arr)->size - (i)) * (arr)->elem_size)
-#define arr_dest(arr) free((arr)->data)
-#define arr_qsort(arr, cmp) qsort((arr)->data, (arr)->size, (arr)->elem_size, cmp)
-#define arr_bsearch(arr, key, cmp) bsearch(key, (arr)->data, (arr)->size, (arr)->elem_size, cmp)
+#define arr_remove_at(arr, i) memmove(arr + (i), arr + (i+1), (--arr_data(arr)->size - (i)) * sizeof(*arr))
+#define arr_dest(arr) free(arr_data(arr))
+#define arr_qsort(arr, cmp) qsort(arr, arr_len(arr), sizeof(*arr), cmp)
+#define arr_bsearch(arr, key, cmp) bsearch(key, arr, arr_size(arr), cmp)
 #endif  /* ARRAY_H */
