e65929007059aba77a0e07bd4ab8e9a33b7b4223
Sidd <sidd.s.pai@gmail.com>
2023-10-06 21:08:21 -0600

    Added `arr_fill` back again and `reverse` macro.

--- array.h
+++ array.h
@@ -10,33 +10,38 @@ typedef struct { size_t size, capacity; } _array;
 #define arr_data(arr) (((_array *) arr) - 1) 
 #define arr_new(T, ...) ({                                 \
     T tmp[] = { __VA_ARGS__ };                             \
-    char *ptr = malloc(sizeof(_array) + max(8 * sizeof(T), sizeof(tmp))); \
-    ptr += sizeof(_array);                                 \
+    _array *ptr = malloc(sizeof(_array) + max(8 * sizeof(T), sizeof(tmp))); \
+    ptr++;                                                 \
     arr_data(ptr)->size = sizeof(tmp) / sizeof(T);         \
     arr_data(ptr)->capacity = max(8 * sizeof(T), sizeof(tmp)) / sizeof(T); \
-    memcpy(ptr, tmp, sizeof(tmp));                         \
+    (T *) memcpy(ptr, tmp, sizeof(tmp));                   \
+})
+#define arr_fill(T, val, len) ({                           \
+    _array *ptr = malloc(sizeof(_array) + len * sizeof(T));\
+    ptr++;                                                 \
+    arr_data(ptr)->size = len;                             \
+    arr_data(ptr)->capacity = len;                         \
+    for (size_t i = 0; i < len; ++i)                       \
+        ((T *) ptr)[i] = val;                              \
     (T *) ptr;                                             \
 })
 #define each(item, arr) (typeof(arr) item = arr; item != &arr[len(arr)]; ++item)
+#define reverse(item, arr)  (typeof(arr) item = &arr[len(arr)-1]; item != arr - 1; --item)
 #define len(arr) ({ arr_data(arr)->size; })
 #define cap(arr) ({ arr_data(arr)->capacity; })
 #define arr_resize(arr, new_capacity) do {                 \
     arr_data(arr)->capacity = new_capacity;                \
     _array *tmp = realloc(arr_data(arr), sizeof(_array) + new_capacity * sizeof(*arr)); \
-    arr = (typeof(arr)) (((char *) tmp) + sizeof(_array)); \
+    arr = (typeof(arr)) (++tmp);                           \
 } while (0)
-#define arr_push(arr, elem) do {                           \
-    if (cap(arr) == len(arr))                              \
-        arr_resize(arr, cap(arr) * 2);                     \
-    arr[arr_data(arr)->size++] = elem;                     \
-} while (0)
-#define arr_pop(arr) ({ arr[--arr_data(arr)->size]; })
 #define arr_insert(arr, i, elem) do {                      \
-    if (cap(arr) == len(arr))                              \
+    if (len(arr) == cap(arr))                              \
         arr_resize(arr, cap(arr) * 2);                     \
     memmove(arr + i + 1, arr + i, (arr_data(arr)->size++ - i) * sizeof(*arr)); \
     arr[i] = elem;                                         \
 } while (0)
+#define arr_push(arr, elem) arr_insert(arr, (len(arr) - 1), elem)
+#define arr_pop(arr) ({ arr[--arr_data(arr)->size]; })
 #define arr_remove_at(arr, i) memmove(arr + (i), arr + (i+1), (--arr_data(arr)->size - (i)) * sizeof(*arr))
 #define arr_dest(arr) free(arr_data(arr))
 #define arr_qsort(arr, cmp) qsort(arr, len(arr), sizeof(*arr), cmp)
