6b4798a4db7722d8b12306f8ed55e28f7c89b9aa
Sidd <sidd.s.pai@gmail.com>
2023-12-11 17:24:34 -0700

    Updated code.

--- array.h
+++ array.h
@@ -6,22 +6,23 @@
 
 typedef struct { size_t size, capacity; } array;
 
+#define max(a, b) (((a) > (b)) ? (a) : (b))
 #define array(T) T *
 #define arr_data(arr) (((array *) arr) - 1) 
 #define arr_new(T, ...) ({                                 \
     T tmp[] = { __VA_ARGS__ };                             \
-    array *ptr = malloc(sizeof(array) + sizeof(tmp));      \
-    *ptr = (array) { 0, sizeof(tmp) / sizeof(T) };         \
-    (T *) memcpy(ptr + 1, tmp, sizeof(tmp));               \
+    array *ptr = malloc(sizeof(array) + max(8 * sizeof(T), sizeof(tmp)));                    \
+    *ptr = (array) { sizeof(tmp) / sizeof(T), max(8 * sizeof(T), sizeof(tmp)) / sizeof(T) }; \
+    memcpy(++ptr, tmp, sizeof(tmp));                       \
 })
 #define arr_fill(T, val, len) ({                           \
-    T *arr = (T *) (((char *) malloc(sizeof(array) + len * sizeof(T))) + sizeof(array));  \
-    arr_resize(arr, len);                                  \
-    for each(item, arr)                                    \
-        *item = val;                                       \
-    arr;                                                   \
+    array *ptr = malloc(sizeof(array) + len * sizeof(T));  \
+    ptr++;                                                 \
+    arr_resize(ptr, len);                                  \
+    for (size_t i = 0; i < len; ++i)                       \
+        ((T *) ptr)[i] = val;                              \
+    (T *) ptr;                                             \
 })
-#define arr_from_str(str) strcpy(arr_fill(char, '\0', strlen(str) + 1), str)
 #define each(item, arr) (typeof(arr) item = arr; item != &arr[len(arr)]; ++item)
 #define reversed(item, arr)  (typeof(arr) item = &arr[len(arr)-1]; item != arr - 1; --item)
 #define len(arr) ({ arr_data(arr)->size; })
@@ -35,7 +36,7 @@ typedef struct { size_t size, capacity; } array;
 #define arr_insert(arr, i, elem) do {                      \
     if (len(arr) == cap(arr))                              \
         arr_realloc(arr, cap(arr) * 2);                    \
-    arr[i] = (memmove(arr + i + 1, arr + i, (arr_data(arr)->size++ - i) * sizeof(*arr)), elem); \
+    arr[i] = (memmove(&arr[(i)+1], &arr[i], (arr_data(arr)->size++ - (i)) * sizeof(*arr)), elem); \
 } while (0)
 #define make_cmp(T) ({                          \
     int __cmp__(const void *a, const void *b) { \
@@ -48,10 +49,8 @@ typedef struct { size_t size, capacity; } array;
     typeof(*arr) tmp = value;                   \
     bsearch(&tmp, arr, len(arr), sizeof(*arr), make_cmp(typeof(*arr))); \
 })
-#define arr_push(arr, elem) arr_insert(arr, (len(arr)-1), elem)
+#define arr_push(arr, elem) arr_insert(arr, (len(arr) - 1), elem)
 #define arr_pop(arr) ({ arr[--arr_data(arr)->size]; })
-#define arr_remove_at(arr, i) memmove(arr+(i), arr+(i+1), (--arr_data(arr)->size - (i)) * sizeof(*arr))
+#define arr_remove_at(arr, i) (memmove(&arr[i], &arr[(i)+1], (--arr_data(arr)->size - (i)) * sizeof(*arr)))
 #define arr_free(arr) free(arr_data(arr))
-#define arr_qsort(arr, cmp) qsort(arr, len(arr), sizeof(*arr), cmp)
-#define arr_bsearch(arr, key, cmp) bsearch(key, arr, len(arr), sizeof(*arr), cmp)
 #endif  /* ARRAY_H */
